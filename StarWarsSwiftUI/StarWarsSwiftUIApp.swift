889c7aac43fa3e904ae12a28eaec6e8ac16674a9
Maksim Skrypka <skrypka.m.l@gmail.com>
2022-08-15 12:40:02 +0300

    Initial Commit

---
+++ StarWarsSwiftUI/StarWarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSwiftUIApp.swift
+//  StarWarsSwiftUI
+//
+//  Created by Maksim Skrypka on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
